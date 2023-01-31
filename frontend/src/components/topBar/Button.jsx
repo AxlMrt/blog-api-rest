deeba93af796b9eb811f3ff890a96266e5ce84a7
Axel <martin.axel@sfr.fr>
2023-01-31 08:24:03 +0100

    change color of hovering sidebar

--- frontend/src/components/topBar/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState, useContext } from 'react';
-import { Context } from '../../context/Context';
-
-function Button() {
-  const ctx = useContext(Context);
-  return <button onClick={ctx.toggleMenu}>Toggle menu</button>;
-}
-
-export default Button;
